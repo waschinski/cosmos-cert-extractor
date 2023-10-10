391b1ee6dd28a5d5743ea5d3c8f8fe7c88ff1347
Andreas Waschinski <25221082+waschinski@users.noreply.github.com>
2023-10-10 23:41:09 +0200

    Update extract.py

--- extract.py
+++ extract.py
@@ -1,13 +1,16 @@
 import json
 
-with open("/input/cosmos.config.json", "r") as conf_file:
-    config_object = json.load(conf_file)
+try:
+    with open("/input/cosmos.config.json", "r") as conf_file:
+        config_object = json.load(conf_file)
+except OSError:
+    print("Couldn't read the config file.")
 
 cert = config_object["HTTPConfig"]["TLSCert"]
 key = config_object["HTTPConfig"]["TLSKey"]
 
-with open("/output/cert.pem", "w") as cert_file:
+with open("/output/certs/cert.pem", "w") as cert_file:
     cert_file.write(cert)
 
-with open("/output/key.pem", "w") as key_file:
+with open("/output/certs/key.pem", "w") as key_file:
     key_file.write(key)
