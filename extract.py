33dec69213f0203af6c1eda1b5af9b4f51119921
lilkidsuave <66171643+lilkidsuave@users.noreply.github.com>
2024-06-20 00:43:35 -0400

    bug fix

--- extract.py
+++ extract.py
@@ -118,7 +118,7 @@ def main():
             current_time = time.time()
             cert_data, key_data = load_certificates()
             # Condition to renew certificates if expired or interrupted
-            if is_cert_expired(cert_data) or (interrupted and check_interval > 0):
+            if is_cert_expired(cert_data) or interrupted and check_interval > 0:
                 renew_certificates()
                 print(f'Updating again in {check_interval} seconds.')
                 next_check_time = current_time + check_interval  # Update next_check_time
@@ -130,7 +130,7 @@ def main():
                 next_check_time = current_time + check_interval
 
             # Handle the case when CHECK_INTERVAL is 0 and certificate expired or interrupted
-            if check_interval == 0 and (is_cert_expired(cert_data) or interrupted):
+            if check_interval == 0 and is_cert_expired(cert_data) or interrupted:
                 renew_certificates()
 
             time.sleep(1)  # Sleep for 1 second between iterations
